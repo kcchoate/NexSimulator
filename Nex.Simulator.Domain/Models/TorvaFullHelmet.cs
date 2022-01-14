62bba5dc74983b2b3659098ef1918969ad15e0f9
Kendrick Choate <kendrick.choate@7factor.io>
2022-01-13 19:37:45 -0600

    Fix Name for Torva FullHelm

--- Nex.Simulator.Domain/Models/TorvaFullHelmet.cs
+++ Nex.Simulator.Domain/Models/TorvaFullHelmet.cs
@@ -2,6 +2,6 @@ namespace Nex.Simulator.Domain.Models;
 
 public record TorvaFullHelmet : NexUnique
 {
-    public override string Name { get; } = "Torva PlateLegs";
+    public override string Name { get; } = "Torva Full Helm";
     public override decimal DropRate { get; } = new decimal(2) / new decimal(12);
 }
