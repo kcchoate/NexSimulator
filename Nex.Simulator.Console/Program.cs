673184ec71ddc0408640345e017e1902b7f05f06
Kendrick Choate <kendrick.choate@7factor.io>
2022-01-13 20:50:50 -0600

    Add percentile based metrics to console app

--- Nex.Simulator.Console/Program.cs
+++ Nex.Simulator.Console/Program.cs
@@ -43,20 +43,39 @@ static IEnumerable<int> GetStatsForUniques(
         results.Add(nexSimulator.GetKillsForUniques(teamSize, desiredUniques));
     }
 
-    Console.WriteLine($"Team Size: {teamSize}");
-    Console.WriteLine($"Total Iterations: {iterations}");
-    Console.WriteLine($"Desired Uniques: {string.Join(",", desiredUniques.Select(x => x.Name))}");
-    Console.WriteLine($"Average Kills Required: {results.Average(x => x.Kills)}");
-    Console.WriteLine($"Minimum Kills Required: {results.Min(x => x.Kills)}");
-    var maxKills = results.MaxBy(x => x.Kills);
-    var maxKillUniques = maxKills!.Uniques.GroupBy(x => x.Name);
-    Console.WriteLine($"Maximum Kills Required: {maxKills.Kills}");
-    Console.WriteLine($"The guy who got {maxKills.Kills} received:");
-    foreach (var unique in maxKillUniques)
-    {
-        Console.WriteLine($"\t{unique.Count()} {unique.First().Name}");
-    }
-    Console.WriteLine($"Most Common # Kills Required: {results.GroupBy(x => x.Kills).MaxBy(x => x.Count())!.Key}");
+    PrintBasicStatistics();
+    PrintPercentileBreakdown();
 
     return results.Select(x => x.Kills);
+
+    void PrintBasicStatistics()
+    {
+        Console.WriteLine($"Team Size: {teamSize}");
+        Console.WriteLine($"Total Iterations: {iterations}");
+        Console.WriteLine($"Desired Uniques: {string.Join(",", desiredUniques.Select(x => x.Name))}");
+        Console.WriteLine($"Average Kills Required: {results.Average(x => x.Kills)}");
+        Console.WriteLine($"Minimum Kills Required: {results.Min(x => x.Kills)}");
+        var maxKills = results.MaxBy(x => x.Kills);
+        var maxKillUniques = maxKills!.Uniques.GroupBy(x => x.Name);
+        Console.WriteLine($"Maximum Kills Required: {maxKills.Kills}");
+        Console.WriteLine($"The guy who got {maxKills.Kills} received:");
+        foreach (var unique in maxKillUniques)
+        {
+            Console.WriteLine($"\t{unique.Count()} {unique.First().Name}");
+        }
+
+        Console.WriteLine($"Most Common # Kills Required: {results.GroupBy(x => x.Kills).MaxBy(x => x.Count())!.Key}");
+    }
+
+    void PrintPercentileBreakdown()
+    {
+        var orderedKcs = results.OrderBy(x => x.Kills).ToList();
+        var percentiles = Enumerable.Range(1, 9).Select(x => x * .1);
+        foreach (var percentile in percentiles)
+        {
+            var kcInPercentile = orderedKcs.Skip(Convert.ToInt32(iterations * percentile)).First().Kills;
+            Console.WriteLine($"{percentile * 100}% of people received their uniques within {kcInPercentile} KC");
+        }
+        Console.WriteLine($"100% of people received their uniques within {results.Count} KC");
+    }
 }
